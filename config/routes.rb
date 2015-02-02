194c8b2ccc38803da4ca802ca0e608413bc14187
Andrew J Dickson <andrewdickson23@gmail.com>
2015-02-02 17:36:29 -0500

    pre-bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 UpstateSyrup::Application.routes.draw do
   resources :sites
 
-  root to: 'sites#index', as: 'home'
+  #root to: 'sites#index', as: 'home'
 
   get 'pages/home' => 'high_voltage/pages#show', id: 'home'
 
