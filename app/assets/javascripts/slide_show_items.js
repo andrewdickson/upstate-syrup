8d414922f9bb83967bf0310db38091b26b7b53d5
Andrew J Dickson <andrewdickson23@gmail.com>
2015-03-15 15:51:08 -0400

    debugging

--- app/assets/javascripts/slide_show_items.js
+++ app/assets/javascripts/slide_show_items.js
@@ -36,12 +36,12 @@ function setIndexEvents(){
 }
 
 
-$(document).ready(function(){
+function setFileUpload(selector){
 
     var max_height = $('#slideshow_form').data('max-height');
     var max_width = $('#slideshow_form').data('max-width');
 
-    $('.file-upload').fileupload({
+    $(selector).fileupload({
 
         url: $('#slideshow_form').data('url'),
         dataType: 'script',
@@ -54,7 +54,7 @@ $(document).ready(function(){
         add: function(e,data){
             var current_data = $(this);
             data.process(function(){
-               return current_data.fileupload('process',data);
+                return current_data.fileupload('process',data);
             }).done(function(){
                 data.submit();
             });
@@ -65,47 +65,50 @@ $(document).ready(function(){
             fileTypes: /^image\/(gif|jp?g)$/,
             maxFileSize: 20000000},
             {action: "resize",
-            imageMaxWidth: 1500,
-            imageMaxHeight: 500},{
-            action: "save"}]
+                imageMaxWidth: 1500,
+                imageMaxHeight: 500},{
+                action: "save"}]
 
     });
+}
 
-/*    $('img#test').css('max-height', max_height + 'px');
-    $('img#test').css('max-width', max_width + 'px');
-    $('img#test').css('min-height', min_height + 'px');
-    $('img#test').css('min-width', min_width + 'px');
+$(document).ready(function(){
 
-    $('img#test').attr('style', 'max-height: ' + max_height + 'px;');
-
-
-    var preview = $(".upload-preview img");
-
-    $(".file").change(function(event){
-        var input = $(event.currentTarget);
-        var file = input[0].files[0];
-        var reader = new FileReader();
-        reader.onload = function(e){
-            image_base64 = e.target.result;
-            preview.attr("src", image_base64);
-        };
-
-        reader.readAsDataURL(file);
-        /*var check = function(condition){
-            if(condition){
-                // run when condition is met
-            }
-            else {
-                setTimeout(check, 1000); // check again in a second
-            }
-        }
-
-        check(true);
-        check(false);
-
-    });*/
+    if($('.file-upload').length)
+        setFileUpload('.file-upload');
 
     setIndexEvents();
 
 
+    /*
+     $('.file-upload').fileupload({
+
+     url: $('#slideshow_form').data('url'),
+     dataType: 'script',
+     disableImageResize: false,
+     imageMaxWidth: max_width,
+     imageMaxHeight: max_height,
+     uploadTemplateId: null,
+     downloadTemplateId: null,
+
+     add: function(e,data){
+     var current_data = $(this);
+     data.process(function(){
+     return current_data.fileupload('process',data);
+     }).done(function(){
+     data.submit();
+     });
+
+     },
+     process: [{
+     action: "load",
+     fileTypes: /^image\/(gif|jp?g)$/,
+     maxFileSize: 20000000},
+     {action: "resize",
+     imageMaxWidth: 1500,
+     imageMaxHeight: 500},{
+     action: "save"}]
+
+     });
+     */
 });
