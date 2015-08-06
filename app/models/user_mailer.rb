ae2586c3268858e67e23b1065eed4b4fc40c5967
Andrew Dickson <andrewdickson23@gmail.com>
2015-08-05 22:32:57 -0400

    debugging email set dynamically

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -8,6 +8,7 @@ class UserMailer < ActionMailer::Base
     @email = email
     @DTTM =  DateTime.now.in_time_zone("EST").strftime("%B %-d, %Y %I:%M%P")
 
+    puts SettingUtility.settings['message_cc']
     mail(from: ENV['system_email_from'], to: ENV['feedback_email'], cc: SettingUtility.settings['message_cc'], bcc: ENV['feedback_email_bcc'], subject: "Syrup Website [#{full_name}]")
   end
 
