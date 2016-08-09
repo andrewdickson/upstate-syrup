a66b0101d87aa0e6760944e57bc65c06921e3060
Andrew Dickson <andrewdickson23@gmail.com>
2016-08-08 21:21:49 -0400

    troubleshooting

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
      var address = encodeURIComponent(address_data); //'3164 state route 26 Constableville, NY 13325, USA'
 
      $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+address+'&sensor=false', null, function (data) {
-         var p = data.results[0].geometry.location
+         var p = data.results[0].geometry.location;
          var latlng = new google.maps.LatLng(p.lat, p.lng);
          var markers = new google.maps.Marker({
              position: latlng,
@@ -36,10 +36,10 @@ $(document).ready(function(){
          setTimeout(function(){google.maps.event.removeListener(zoomChangeBoundsListener)}, 2000);
 
 
-     /*    var infowindow = new google.maps.InfoWindow({
-             content:"Kraeger Syrup"
+         var infowindow = new google.maps.InfoWindow({
+             content:"Kraeger's Maple Syrup"
          });
-         infowindow.open(map,markers[0]); todo: label with brand */
+         infowindow.open(map,markers[0]); //todo: label with brand
      });
 
 });
